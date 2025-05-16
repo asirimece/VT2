608d0b0e39093e51f53ac703be7667719a5dad17
asirimece <asirimece@gmail.com>
2025-05-16 16:01:50 +0000

    left vs right - 2 class exp

--- src/transfer/run_transfer.py
+++ src/transfer/run_transfer.py
@@ -19,14 +19,14 @@ def run(config: DictConfig) -> None:
      #preprocessed_data = preprocessor.run()
      #save_preprocessed_data(preprocessed_data, config.dataset.preprocessing.output_file)
 
-     with open("./dump/preprocessed_data.pkl", "rb") as f:
+     with open("./dump/1vs2/preprocessed_data.pkl", "rb") as f:
           preprocessed_data = pickle.load(f)
           
      #features = FeatureExtractor.run(config, preprocessed_data)
      #save_features(features, config.transform.output_file)
 
-     with open("./dump/features.pkl", "rb") as f:
-          preprocessed_data = pickle.load(f)
+     with open("./dump/1vs2/features.pkl", "rb") as f:
+          features = pickle.load(f)
           
      trainer = MTLTrainer(config.experiment, config.model)
      mtl_wrapper = trainer.run()
